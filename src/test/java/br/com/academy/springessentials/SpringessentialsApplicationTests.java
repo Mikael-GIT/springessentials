0101c6e1b809b0f9b352bdf9110caf981b6672e1
Mikael Tavares <mikael.tavares@unigranrio.br>
2023-01-22 00:39:58 -0300

    first commit

---
+++ src/test/java/br/com/academy/springessentials/SpringessentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academy.springessentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringessentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
